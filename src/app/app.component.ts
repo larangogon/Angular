63a248c557f757e7b21ebc2fd6891d077373998a
larangogon <67110144+larangogon@users.noreply.github.com>
2021-02-17 22:26:31 -0500

    feat: se agregan modificaciones a componentes e interface para productos. agregar y eliminar objetos y crear array de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectAngular';
+  names = [];
+  name: string;
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.jpg',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.jpg',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.jpg',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.jpeg',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.jpeg',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.jpeg',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addName(name: string) {
+    this.names.push(name);
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
+
 }
